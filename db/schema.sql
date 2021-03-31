750a43e5e64d2761b808d62c51665647d4106ed1
Noor Abdikadir <nooromar305@gmail.com>
2021-03-30 23:20:04 -0700

    Added family names as EMployeers for example data... for the sake of science

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,11 @@
+/* 
+* **role**:
+  * **id** - INT PRIMARY KEY
+  * **title** -  VARCHAR(30) to hold role title
+  * **salary** -  DECIMAL to hold role salary
+  * **department_id** -  INT to hold reference to department role belongs to
+*/
+
 DROP DATABASE IF EXISTS employeeDb;
 
 CREATE DATABASE employeeDb;
--- db/seed.sql
+++ db/seed.sql
@@ -10,19 +10,30 @@ INSERT INTO departments (name) VALUES
 ("Engineering"),
 ("Human-resources");
 
---Assigning the roles to title, salary, and department id
+--Assigning the roles 
 INSERT INTO role (title, salary, department_id) VALUES
-("Accountanting Manager", 100000, 1), --title/salary/id
+("Accountanting Manager", 100000, 1), --Sequence - title/salary/id
 ("Financial Associate", 62000, 1),
-("Sales Manager", 100000, 1),
-("Sales Associate", 100000, 1),
-("Operations Manager", 100000, 1),
-("Operations Associate", 100000, 1),
-("Chef Engineer", 100000, 1),
-("Engineer", 100000, 1),
-("HR Manager", 100000, 1),
-("HR Associate", 100000, 1);
+("Sales Manager", 90000, 2),
+("Sales Associate", 50000, 2),
+("Operations Manager", 80000, 3),
+("Operations Associate", 50000, 3),
+("Chef Engineer", 110000, 4),
+("Engineer", 100000, 4),
+("HR Manager", 80000, 5),
+("HR Associate", 60000, 5);
 
---Employees Data
+--Employees Data - repetiive last names due to using family names as examples
 INSERT INTO employees(first_name, role_id, manager_id,department_id) VALUES
+("Noor", "Abdikadir", 7, NULL, 4),
+("Mohamed", "Tasir", 5, NULL, 3),
+("Abdi", "Ali", 8, 1, 4),
+("Harun", "Abdi", 1, NULL, 1),
+("Malyun", "Abdi", 3, NULL, 2),
+("Mette", "Hussein", 6, 2, 3),
+("Zeinab", "Aden", 2, 4, 1),
+("Sahara", "Aden", 4, 5, 2),
+("Hassan", "Shungolo", 9, NULL, 5),
+("Nima", "Abdi", 10, 9, 5);
+
 
